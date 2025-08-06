7031b366af5d4bfcb0b527b40b67a879a66f6154
HyagoIFSilva <hyagodw32@outlook.com>
2025-08-06 15:09:52 -0300

    Commit Inicial

---
+++ App.js
@@ -0,0 +1,68 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Pressable, } from 'react-native';
+import { useState } from 'react';
+
+
+export default function App() {
+  return (
+    <View style={styles.geral}>
+
+
+    <View style={styles.elementos}>
+      <View style={styles.box}>
+
+      </View>
+      <View style={styles.box}>
+
+      </View>
+      <View style={styles.box}>
+
+      </View>
+      <View style={styles.box}>
+
+      </View>
+
+              </View>
+      <StatusBar style="auto" />
+
+    </View>
+
+
+
+
+  );
+}
+
+const styles = StyleSheet.create({
+  geral: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: 'black', 
+    borderRadius: 10,
+    width: '90%',
+    height: '100%',  
+  },
+  elementos: {
+    display: 'flex',
+    direction: 'row',
+    justifyContent: 'space-around',
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '90%',
+    height: '50%',
+  },
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 10,
+    padding: 5,
+  },
+});
