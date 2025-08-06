792bafa4fd7c03d4daf17f5c1f894057d73d7d04
HyagoIFSilva <hyagodw32@outlook.com>
2025-08-06 16:48:46 -0300

    UPDATE

--- App.js
+++ App.js
@@ -6,7 +6,23 @@ import { TextInput } from 'react-native-web';
 
 export default function App() {
 
-  
+    const [corSombraCard, setCorSombraCard] = useState("#fff");
+  const [sombraTop, setSombraTop] = useState(-10);
+
+  const Pressionado = () => {
+    setCorSombraCard("red");
+    setSombraTop(0)
+  };
+
+  const solto = () => {
+    setCorSombraCard("blue");
+     setSombraTop(-10)
+  };
+
+  const funcao = () => {
+    console.log("Card clicado!");
+  };
+
 function addLista() {
   setLista ([...lista, {key: Math.random().toString(), value: numero}]
 );
@@ -22,7 +38,15 @@ const GerarSenha = () => {
   return (
     
     <View style={styles.geral}>
-
+    <View style={styles.sombra}>
+<Pressable
+          style={[styles.card, {top: sombraTop}]}
+          onPressIn={Pressionado}
+          onPressOut={solto}
+          onPress={funcao}
+        ><Text style={styles.texto}>Clique Aqui</Text>
+        </Pressable>
+        </View>
 
     <View style={styles.elementos}>
       <View style={styles.box}>
@@ -52,6 +76,7 @@ const GerarSenha = () => {
 
 const styles = StyleSheet.create({
   geral: {
+    display: 'flex',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -64,23 +89,28 @@ const styles = StyleSheet.create({
   },
   elementos: {
     display: 'flex',
-    direction: 'row',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
     justifyContent: 'space-around',
     flex: 1,
     backgroundColor: '#f0f0f0',
     alignItems: 'center',
     justifyContent: 'center',
-    width: '90%',
-    height: '50%',
+    width: '100%',
+    height: '100%',
+    padding: 30,
   },
   box: {
-    width: 100,
-    height: 100,
+    width: 70,
+    height: 70,
     backgroundColor: 'blue',
     borderWidth: 2,
     borderColor: 'black',
     borderRadius: 10,
-    padding: 5,
+    padding: 2,
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   texto:{
     color: 'red',
@@ -95,5 +125,28 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingHorizontal: 10,
     width: '80%',
+  },
+
+  botao : {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+   sombra:{
+    width:200,
+    height:100,
+    borderRadius:5,
+    backgroundColor:"#00000080"
+  },
+
+  card: {
+    width:200,
+    height:100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor:"#ccc",
+    borderRadius:5
   },
 });
